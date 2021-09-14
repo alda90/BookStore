03e9955c69e5479e562cddee335b36f766f099bf
Aldair Carrillo <aldacdh904@gmail.com>
2021-09-13 18:04:28 -0700

    Fisr Commit

---
+++ BookStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookStore
+//
+//  Created by Alejandro Gonzalez on 13/09/21.
+//  Copyright © 2021 CGTI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
